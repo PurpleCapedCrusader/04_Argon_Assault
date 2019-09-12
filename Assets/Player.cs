5534fb082a57c497bea54fb31b1dcbb7707d7fd5
Alex <alexcotton@mac.com>
2019-09-11 17:02:28 -0700

    ship movement update

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,15 +5,13 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 20f;
-    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 20f;
-    [Tooltip("Meters/Second")] [SerializeField] float xRange = 17f;
-    [Tooltip("Meters/Second")] [SerializeField] float yRange = 7f;
-    [SerializeField] float positionPitchFactor = -2f;
-    [SerializeField] float controlPitchFactor = -30f;
-    [SerializeField] float positionYawFactor = -1.2f;
-    // [SerializeField] float controlYawFactor = 15f;
-    [SerializeField] float positionRollFactor = -0f;
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 40f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 30f;
+    [Tooltip("Meters/Second")] [SerializeField] float xRange = 13f;
+    [Tooltip("Meters/Second")] [SerializeField] float yRange = 8.5f;
+    [SerializeField] float positionPitchFactor = -1.5f;
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float positionYawFactor = 1f;
     [SerializeField] float controlRollFactor = -30f;
     float xThrow, yThrow, zThrow;
 
@@ -30,18 +28,12 @@ public class Player : MonoBehaviour
     }
     private void processRotation()
     {
-        //float yawDueToPosition = -transform.localPosition.x * positionYawFactor;
-        //float yawDueToControlThrow = xThrow * controlYawFactor;
-
         float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
         float pitchDueToControlThrow = yThrow * controlPitchFactor;
-
-        float rollDueToPosition = transform.localPosition.x * positionRollFactor;
-        float rollDueToControlThrow = xThrow * controlRollFactor;
-
         float pitch = pitchDueToPosition + pitchDueToControlThrow;
         float yaw = transform.localPosition.x * positionYawFactor;
-        float roll = rollDueToPosition + rollDueToControlThrow;
+        float roll = xThrow * controlRollFactor;
+
         transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
     private void processTranslation()
