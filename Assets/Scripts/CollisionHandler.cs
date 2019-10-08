9caa9d0350262c747e22c8bbb57ec9944ae6ffa8
Alex <alexcotton@mac.com>
2019-10-08 16:57:58 -0700

    added deathFX

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CollisionHandler : MonoBehaviour
-{
+public class CollisionHandler : MonoBehaviour {
+
+    [Tooltip("In Seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")][SerializeField] GameObject deathFX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // deathFX = GameObject.Find("/MainCamera/PlayerShip/Explosion");
     }
 
     void OnTriggerEnter(Collider other)
@@ -17,11 +21,18 @@ public class CollisionHandler : MonoBehaviour
     private void StartDeathSequence()
     {
         print("CollisionHandler.StartDeathSequence");
+        deathFX.SetActive(true);
         SendMessage("OnPlayerDeath");
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+    private void LoadFirstLevel() // string referenced
+    {
+        SceneManager.LoadScene(1);
     }
     // Update is called once per frame
     void Update()
     {
-        
+     
     }
 }
