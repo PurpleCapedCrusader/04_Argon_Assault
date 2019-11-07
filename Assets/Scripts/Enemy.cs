9891512f9b399a215fef8eed8348cdb93ae3cc21
Alex <alexcotton@mac.com>
2019-11-07 13:45:13 -0800

    Move score to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
     ScoreBoard scoreBoard;
     // Start is called before the first frame update
     void Start()
@@ -23,7 +24,7 @@ public class Enemy : MonoBehaviour
     void OnParticleCollision(GameObject other)
     {
         print("Particles collided with emeny" + gameObject.name);
-        ScoreHit();
+        scoreBoard.ScoreHit(scorePerHit);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
